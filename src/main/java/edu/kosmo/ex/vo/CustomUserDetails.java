e423099f3553a4fe84363cc24b5ba8f4cd01b781
DESKTOP-90VMTD4\KOSMO <dmsdlz@naver.com>
2022-01-10 11:34:34 +0900

    commit

---
+++ src/main/java/edu/kosmo/ex/vo/CustomUserDetails.java
@@ -0,0 +1,134 @@
+package edu.kosmo.ex.vo;
+
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.kosmo.ex.mapper.EmpMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Getter
+@Setter
+public class CustomUserDetails implements UserDetails {
+	
+	
+	//=================================================================================================
+	private EmpVO emp;
+	
+	// ================================================================================================
+	private String password;
+	private final String username;
+	private final Collection<? extends GrantedAuthority> authorities;
+	private final boolean accountNonExpired;
+	private final boolean accountNonLocked;
+	private final boolean credentialsNonExpired;
+	private final boolean enabled;
+
+	// ~ Constructors
+	// ===================================================================================================
+	
+	public CustomUserDetails(EmpVO empVO) {
+		 //Integer.toString(empVO.getEmpno()),getAuth(empVO)
+		
+		this(empVO.getEname(), Integer.toString(empVO.getEmpno()), true, true, true, true, getAuth(empVO));
+		this.emp = empVO;	
+	}
+	
+	public CustomUserDetails(String username, String password, boolean enabled,
+			boolean accountNonExpired, boolean credentialsNonExpired,
+			boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+
+		if (((username == null) || "".equals(username)) || (password == null)) {
+			throw new IllegalArgumentException(
+					"Cannot pass null or empty values to constructor");
+		}
+
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.accountNonExpired = accountNonExpired;
+		this.credentialsNonExpired = credentialsNonExpired;
+		this.accountNonLocked = accountNonLocked;
+		this.authorities = authorities;
+	}
+	
+	//유저가 갖고 있는 권한 목록
+	public static Collection<? extends GrantedAuthority> getAuth(EmpVO empVO) { 
+
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		
+		for (AuthVO auth: empVO.getAuthList()) {
+			authorities.add(new SimpleGrantedAuthority(auth.getAuthority()));
+		}
+		
+		return authorities;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return accountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return accountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return credentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+	
+
+}

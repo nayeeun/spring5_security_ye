b78d9abbd3e940763647550b8a6543fef39bdcad
DESKTOP-90VMTD4\KOSMO <dmsdlz@naver.com>
2022-01-10 12:45:30 +0900

    commit

--- src/main/java/edu/kosmo/ex/HomeController.java
+++ src/main/java/edu/kosmo/ex/HomeController.java
@@ -1,12 +1,16 @@
 package edu.kosmo.ex;
 
 
+import java.security.Principal;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.kosmo.ex.controller.SecurityController;
+import edu.kosmo.ex.vo.CustomUserDetails;
 import lombok.extern.log4j.Log4j;
 
 /**
@@ -59,5 +64,33 @@ public class HomeController {
 		log.info("Welcome Login Form");
 		return "/login/loginForm2";
 	}
+	
+	@RequestMapping(value = "/loginInfo", method = RequestMethod.GET)
+	   public String loginInfo(Principal principal,Model model) {
+	      
+	      //1.Controller를 통하여 Pincipal객체로 가져오는 방법
+	      String user_id = principal.getName();
+	      System.out.println("유저 아이디:" + user_id   );
+	      
+	      //2.SpringContextHolder를 통하여 가져오는 방법(일반적인 빈에서 사용 할수있음 )
+	      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	        user_id = auth.getName();
+	        System.out.println("유저 아이디:" + user_id   );
+	        
+	      //3.
+	      UserDetails userDetails = (UserDetails) auth.getPrincipal();
+	      System.out.println(userDetails.getUsername());
+
+	      //4.
+	      CustomUserDetails couCustomUserDetails =  (CustomUserDetails) auth.getPrincipal();
+	      System.out.println(couCustomUserDetails.getEmp());
+	        
+	      //5.User 클래스로 변환 하여 가져오는 방법
+	      couCustomUserDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	      user_id = couCustomUserDetails.getUsername();
+	      System.out.println("유저 아이디:" + user_id   );
+	        
+	      return "home";
+	 }
 	
 }
